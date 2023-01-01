a4007f0bed28253b025ebf19a7a36bd98eb59833
mhamad <mhamad.hmd20@gmail.com>
2023-01-01 20:07:42 +0200

    pc

--- src/components/sword/sword.tsx
+++ src/components/sword/sword.tsx
@@ -16,22 +16,19 @@ const Sword = () => {
         let mouseX = sword.current!.getBoundingClientRect().right;
         let mouseY = sword.current!.getBoundingClientRect().top;
         let radianDegrees = Math.atan2(e.clientX - mouseX, e.clientY - mouseY);
-        rotationDegrees = radianDegrees * (180 / Math.PI) * -1 - 10;
+        rotationDegrees = radianDegrees * (180 / Math.PI) * -1 -10;
         sword.current!.style.transform = `rotate(${rotationDegrees}deg)`
-        swordEffect.current!.style.transform = `rotate(${rotationDegrees}deg)`
     }
     
     const swordAttack = (e: any) => {
         console.log(swordRotation)
-        sword.current!.style.translate = `${e.x + 10}px ${e.y - 130}px`
-        swordEffect.current!.classList.add('effectActive')
-        swordEffect.current!.style.translate = `${e.x + 10}px ${e.y - 130}px`
-
-        const resetSword = setTimeout(() => {
-            sword.current!.style.translate = `50vw`
-            swordEffect.current!.style.translate = `50vw`
-            swordEffect.current!.classList.remove('effectActive')
-        }, 2000)
+        sword.current!.style.translate = `${e.x}px ${e.y - 200}px`
+        sword.current!.classList.add('swordAnimate')
+        
+        // const resetSword = setTimeout(() => {
+        //     sword.current!.classList.remove('swordAnimate')
+        //     sword.current!.style.translate = `50vw`
+        // }, 2000)
 
 
     }
@@ -49,10 +46,10 @@ const Sword = () => {
 
 
     return (
-        <>
-            <img ref={sword} className='swordMedia sword' src={swordImg} alt="" />
-            <div ref={swordEffect} className="swordEffect"></div>
-        </>
+        <div ref={sword} className='sword' >
+            {/* <span ref={swordEffect} className="swordEffect"></span> */}
+            <img  className='swordMedia ' src={swordImg} alt="" />
+        </div>
     )
 }
 
