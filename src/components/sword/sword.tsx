1b3cb5d93679c851b6faf8555616b6a358cfc918
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-01-02 13:13:59 +0200

    lptpt

--- src/components/sword/sword.tsx
+++ src/components/sword/sword.tsx
@@ -1,5 +1,6 @@
 import './sword.scss'
 import swordImg from '../../assets/bankaiSword.png'
+import polyGon from '../../assets/Polygon.png'
 
 import React, { useEffect, useRef, useState } from 'react'
 
@@ -12,36 +13,39 @@ const Sword = () => {
     const [swordRotation, setSwordRotation] = useState(Number)
     let rotationDegrees = 0
 
-    const swordTarget = (e: any) => {
-        let mouseX = sword.current!.getBoundingClientRect().right;
-        let mouseY = sword.current!.getBoundingClientRect().top;
+    const swordTarget = (e: any, element) => {
+        let mouseX = element.current!.getBoundingClientRect().right;
+        let mouseY = element.current!.getBoundingClientRect().top;
         let radianDegrees = Math.atan2(e.clientX - mouseX, e.clientY - mouseY);
-        rotationDegrees = radianDegrees * (180 / Math.PI) * -1 - 10;
-        sword.current!.style.transform = `rotate(${rotationDegrees}deg)`
-        swordEffect.current!.style.transform = `rotate(${rotationDegrees}deg)`
+        rotationDegrees = radianDegrees * (180 / Math.PI) * -1;
+        element.current!.style.transform = `rotate(${rotationDegrees}deg)`
     }
     
     const swordAttack = (e: any) => {
-        console.log(swordRotation)
-        sword.current!.style.translate = `${e.x + 10}px ${e.y - 130}px`
+        sword.current!.style.translate = `${e.x + 50}px ${e.y - 130 }px`
+        swordEffect.current!.style.height = `${e.y +200}px`
+        sword.current!.classList.add('swordActive')
         swordEffect.current!.classList.add('effectActive')
-        swordEffect.current!.style.translate = `${e.x + 10}px ${e.y - 130}px`
+        setTimeout(() => {
+            swordEffect.current!.classList.remove('effectActive')
+            swordEffect.current!.style.height = `252px`
+        }, 500)
 
         const resetSword = setTimeout(() => {
             sword.current!.style.translate = `50vw`
-            swordEffect.current!.style.translate = `50vw`
-            swordEffect.current!.classList.remove('effectActive')
+            // swordEffect.current!.style.translate = `50vw 200px`
         }, 2000)
 
 
     }
 
     useEffect(() => {
-        window!.addEventListener("mousemove", (e) => swordTarget(e));
+        window!.addEventListener("mousemove", (e) => swordTarget(e, sword));
+        window!.addEventListener("mousemove", (e) => swordTarget(e, swordEffect));
 
         window.addEventListener('click', (e) => swordAttack(e))
 
-        return () => {window.removeEventListener("mousemove", (e) => swordTarget(e))}
+        return () => { window.removeEventListener("mousemove", (e) => swordTarget(e, sword)) }
 
     }, [])
 
@@ -50,8 +54,10 @@ const Sword = () => {
 
     return (
         <>
-            <img ref={sword} className='swordMedia sword' src={swordImg} alt="" />
-            <div ref={swordEffect} className="swordEffect"></div>
+            <div className='swordMedia sword' ref={sword}>
+                <img src={swordImg} alt="" />
+            </div>
+            <img ref={swordEffect} className="swordEffect" src={polyGon} alt="" />
         </>
     )
 }
