a0a9f0908b906071819ed4e36f959d462db0461f
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-02-11 16:05:44 +0200

    lptp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header({display, changeDisplay, dark}:any) {
         <div className="headerContainer p-4 transition-all ">
             <div className="headerWrapper flex flex-col items-center  md:items-start gap-4  md:gap-6">
                 <div className="heading flex flex-col w-fit font-bold ">
-                    <h1 className={`title text-4xl md:text-5xl sm:text-4xl ${dark? "text-scndryLightColor" : "text-scndryColor"}  `}>Mohamad Hammoud</h1>
+                    <h1 className={`title text-4xl whitespace-nowrap md:text-5xl sm:text-4xl ${dark? "text-scndryLightColor" : "text-scndryColor"}  `}>Mohamad Hammoud</h1>
                     <span className={`role text-lg text-center ${dark? "text-scndryLightColor" : "text-scndryColor"} `}>Developer & Designer</span>
                 </div>
                 <div className="headerMenus flex justify-center w-full ">
