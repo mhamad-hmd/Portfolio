eb81cd2d75d51c32fd86ff3de5b77361ffb3e622
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-02-11 14:59:31 +0200

    lptp

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import Display from './components/Display/Display'
 import Home from './components/Home/Home'
 import DisplayProjects from './components/DisplayProjects/displayProjects';
 import ContactMe from './components/ContactMe/contactMe';
-import AboutMe from './components/AboutMe/aboutMe';
 import WelcomePage from './components/WelcomePage/WelcomePage';
+import AboutMe from './components/AboutMe/AboutMe';
 
 
 function App() {
@@ -23,21 +23,21 @@ function App() {
     <Router>
       <ParticlesBackground />
       <WelcomePage />
-      <div  className="theme absolute left-0 flex flex-col gap-2">
-        <div className="">
-          <label className='text-white' >Dark</label>
-          <input type="checkbox" />
+      <div  className="theme / absolute top-0 bottom-20 max-w-[9.1vw] w-full /  transition flex flex-col justify-end gap-16 font-bold mainFont  ">
+        <div className=" flex -rotate-90 justify-center items-center gap-2  -ml-4 sm:ml-0 ">
+          <input name='theme' checked={dark} type="radio" onClick={() => setDark(true)} />
+          <label className={`${dark? "text-mainColor" : "text-scndryColor"} transition-all duration-1000 `} >DARK</label>
         </div>
-        <div>
-          <label className="">Light</label>
-          <input type="checkbox" />
+        <div className=' flex -rotate-90 justify-center items-center gap-2 -ml-4 sm:ml-0'>
+          <input name='theme' checked={!dark} type="radio" onClick={() => setDark(false)} />
+          <label className={`${dark? "text-mainColor" : "text-scndryColor"} transition-all duration-1000 `}>LIGHT</label>
         </div>
       </div>
       <div className={`App h-full flex justify-center items-center transition-all ease-out duration-1000 ${dark ? "bg-dark" : "bg-mainColor"}   `}>
         <Routes>
 
           <Route path="/" element={
-            <div className={`mainPageContainer  overflow-hidden relative / flex flex-col gap-2 md:gap-8 / border ${dark ? "border-scndryLightColor" : "border-scndryColor"} h-[95vh] w-[95vw] `}>
+            <div className={`mainPageContainer  overflow-hidden relative / flex flex-col gap-2 md:gap-6 / border ${dark ? "border-scndryLightColor" : "border-scndryColor"} h-[93vh] max-w-[90vw] w-full `}>
               <Header dark={dark} display={display} changeDisplay={changeDisplay} />
               <Display>
                 <Home dark={dark} className={`${display === "Home" ? "scale-100 z-40 opacity-100 translate-x-0" : "scale-60 translate-x-2/3 invisible z-10 opacity-0 pointer-events-none"}`} />
