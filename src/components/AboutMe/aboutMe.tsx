7905a6f0a89b49650a52228e120ecdd42e9d4e9f
mhamad <mhamad.hmd20@gmail.com>
2023-02-12 10:42:11 +0200

    pc

--- src/components/AboutMe/aboutMe.tsx
+++ src/components/AboutMe/aboutMe.tsx
@@ -28,13 +28,13 @@ export default function AboutMe  ({className, dark} : {className:string, dark: b
 
                 <div className={`myInfoWrapper grid gap-4 lg:gap-12 md:gap-8 justify-center items-center `}>
 
-                        <p className={`break-words m-auto max-w-prose w-full ${dark? "text-mainColor" : "text-scndryColor"} scndryFont font-medium text-xl md:text-2xl `}>Started my web development journey by a full stack course on Udemy, and ever since I am acquiring my skills and knowledge to always stay up to date in this field. I am a cooperative worker, and as my projects reveal about me, I am dedicated, responsible and disciplined and also a perfectionist.</p>
+                        <p className={`break-words m-auto max-w-prose w-full ${dark? "text-mainColor" : "text-scndryColor"} scndryFont font-medium text-lg md:text-2xl `}>Started my web development journey by a full stack course on Udemy, and ever since I am acquiring my skills and knowledge to always stay up to date in this field. I am a cooperative worker, and as my projects reveal about me, I am dedicated, responsible, disciplined and a perfectionist.</p>
 
-                    <div ref={myRef} className={`py-4 ${dark? "text-mainColor" : "text-scndryColor"}  w-full `}>
+                    <div ref={myRef} className={`${dark? "text-mainColor" : "text-scndryColor"}  w-full `}>
 
                         <h1 className={`skillTitle mainFont font-bold text-3xl lg:text-4xl `}>Skills :</h1>
 
-                        <p className='scndryFont font-medium text-xl md:text-2xl max-w-prose '>Javascript/ Html5/ css/ React.js/ typescript/ node.js/ shopify/ webpack/ vite/ tailwindcss/ scss/ express.js/ postgresql/ mongodb/ zustand/ redux</p>
+                        <p className='scndryFont font-medium text-lg md:text-2xl max-w-prose '>Javascript/ Html5/ css/ React.js/ typescript/ node.js/ shopify/ webpack/ vite/ tailwindcss/ scss/ express.js/ postgresql/ mongodb/ zustand/ redux</p>
 
                     </div>
 
--- src/components/DisplayProjects/displayProjects.tsx
+++ src/components/DisplayProjects/displayProjects.tsx
@@ -51,8 +51,8 @@ export default function DisplayProjects({ className, dark }: { className: string
 
             <div ref={displayRef} className={`  relative /  items-start justify-start flex flex-col md:items-center md:flex-row gap-4  md:gap-12 / max-w-full  w-full h-full   `}   >
 
-                <div className="myWorkNav flex justify-center items-center whitespace-nowrap  md:h-full w-full md:w-fit ">
-                    <ul className={`  scndryFont   font-medium  / flex md:flex-col gap-8 justify-center p-4 transition-all duration-200  text-xl lg:text-4xl md:text-3xl  ${!category ? "translate-y-[10vh] md:translate-y-0 md:translate-x-[37vw] scale-125 md:scale-110" : "translate-y-0 scale-100 md:translate-x-0"}  `} onClick={() => setMainProject("")}>
+                <div className="myWorkNav flex justify-center items-center md:items-start md:mt-12 whitespace-nowrap  md:h-full w-full md:w-fit ">
+                    <ul className={`  scndryFont   font-medium  / flex md:flex-col gap-8 justify-center p-4 transition-all duration-500 z-20  text-xl lg:text-4xl md:text-3xl  ${!category ? "translate-y-[10vh] md:translate-y-0 md:translate-x-[37vw] scale-125 md:scale-110" : "translate-y-0 scale-100 md:translate-x-0"}  `} onClick={() => setMainProject("")}>
                         <li className={`catigoryTitle ${dark? "text-mainColor" : "text-scndryColor"}  transition-all duration-300 border-b w-fit  ${category == "All" ? " border-scndryColor scale-105" : "border-transparent scale-100"} `} onClick={() => handleNavClick('All')}>All</li>
                         <li className={`catigoryTitle ${dark? "text-mainColor" : "text-scndryColor"} transition-all duration-300 border-b w-fit ${category == "Shopify" ? " border-scndryColor scale-105" : "border-transparent scale-100"} `} onClick={() => handleNavClick("Shopify")}>Shopify</li>
                         <li className={`catigoryTitle ${dark? "text-mainColor" : "text-scndryColor"}   transition-all duration-300 border-b w-fit  ${category == "Full-Stack" ? " border-scndryColor scale-105" : "border-transparent scale-100"} `} onClick={() => handleNavClick('Full-Stack')}>Full-Stack</li>
